40c68695a5a32fa8629839360e2ce7863383dbea
wangbin <NeighborWangShushu@Gmail.com>
2016-07-05 14:07:41 +0800

    create new mine

---
+++ EnergyCycles/Controller/MineVC/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  EnergyCycles
+//
+//  Created by 王斌 on 16/7/5.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface SettingViewController : BaseTableViewController
+
+@end
