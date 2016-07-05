2a929297d02301de5089a9f4f91439645d0a1237
fuer4869 <NeighborWangShushu@Gmail.com>
2016-07-05 14:50:36 +0800

    create new mine

---
+++ EnergyCycles/View/MineView/InviteCodeViewCell.m
@@ -0,0 +1,24 @@
+//
+//  InviteCodeViewCell.m
+//  EnergyCycles
+//
+//  Created by 王斌 on 16/7/5.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "InviteCodeViewCell.h"
+
+@implementation InviteCodeViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
