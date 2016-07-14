01110a966c9435118ecc1370fcbc316d60864028
fuer4869 <fuer4869@gmail.com>
2016-07-14 09:50:34 +0800

    ready to pull

--- EnergyCycles/Controller/TabbarView.m
+++ EnergyCycles/Controller/TabbarView.m
@@ -7,7 +7,7 @@
 //
 
 #import "TabbarView.h"
-
+#import "Masonry.h"
 
 
 @implementation TabbarView
@@ -26,27 +26,18 @@
 
 -(void)layoutView
 {
-    _tabbarView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"tabbar_0"]];
-    [_tabbarView setFrame:CGRectMake(0, 9, _tabbarView.bounds.size.width, 51)];
-    [_tabbarView setUserInteractionEnabled:YES];
     
     _tabbarViewCenter = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"tabbar_mainbtn_bg"]];
-
     _tabbarViewCenter.center = CGPointMake(self.center.x, self.bounds.size.height/2.0);
-    
     [_tabbarViewCenter setUserInteractionEnabled:YES];
     
     _button_center = [UIButton buttonWithType:UIButtonTypeCustom];
     _button_center.adjustsImageWhenHighlighted = YES;
     [_button_center setBackgroundImage:[UIImage imageNamed:@"tabbar_mainbtn"] forState:UIControlStateNormal];
-    
-    [_button_center setFrame:CGRectMake(0, 0, 46, 46)];
-    
     _button_center.center =CGPointMake(_tabbarViewCenter.bounds.size.width/2.0, _tabbarViewCenter.bounds.size.height/2.0 + 5) ;
-    
     [_tabbarViewCenter addSubview:_button_center];
     
-    [self addSubview:_tabbarView];
+    
     [self addSubview:_tabbarViewCenter];
     
     [self layoutBtn];
@@ -56,23 +47,18 @@
 -(void)layoutBtn
 {
     _button_1 = [UIButton buttonWithType:UIButtonTypeCustom];
-//    [_button_1 setBackgroundColor:[UIColor blueColor]];
-    [_button_1 setFrame:CGRectMake(0, 0, 64, 60)];
     [_button_1 setTag:101];
     [_button_1 addTarget:self action:@selector(btn1Click:) forControlEvents:UIControlEventTouchUpInside];
     
     _button_2 = [UIButton buttonWithType:UIButtonTypeCustom];
-    [_button_2 setFrame:CGRectMake(65, 0, 64, 60)];
     [_button_2 setTag:102];
     [_button_2 addTarget:self action:@selector(btn1Click:) forControlEvents:UIControlEventTouchUpInside];
     
     _button_3 = [UIButton buttonWithType:UIButtonTypeCustom];
-    [_button_3 setFrame:CGRectMake(202, 0, 64, 60)];
     [_button_3 setTag:103];
     [_button_3 addTarget:self action:@selector(btn1Click:) forControlEvents:UIControlEventTouchUpInside];
     
     _button_4 = [UIButton buttonWithType:UIButtonTypeCustom];
-    [_button_4 setFrame:CGRectMake(267, 0, 64, 60)];
     [_button_4 setTag:104];
     [_button_4 addTarget:self action:@selector(btn1Click:) forControlEvents:UIControlEventTouchUpInside];
     
@@ -80,6 +66,7 @@
     [_tabbarView addSubview:_button_2];
     [_tabbarView addSubview:_button_3];
     [_tabbarView addSubview:_button_4];
+    
     
 }
 
