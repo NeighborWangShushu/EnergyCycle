b8f55eb97df082bdd0455b21594c184dd06716fa
fuer4869 <NeighborWangShushu@Gmail.com>
2017-05-06 12:52:38 +0800

    在info.plist中添加NSAllowsArbitraryLoadsForMedia

--- EnergyCycles/Tool/AppData/AppNetWork.h
+++ EnergyCycles/Tool/AppData/AppNetWork.h
@@ -48,7 +48,7 @@
 
 //测试环境
 
-#define INTERFACE_URL @"http://192.168.1.111:8888/"
+//#define INTERFACE_URL @"http://192.168.1.111:8888/"
 
 //#define INTERFACE_URL @"http://192.168.1.111:8888/"
 
@@ -59,7 +59,7 @@
 //#define INTERFACE_URL @"http://120.26.218.68:8888/"
 
 //正式环境
-//#define INTERFACE_URL @"http://120.26.218.68:8038/"
+#define INTERFACE_URL @"http://120.26.218.68:8038/"
 
 //阿里云
 //#define INTERFACE_URL @"http://120.26.218.68:8038/"
